c098203fed9aee5c515abdaa615ed21ddd690fec
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-01-13 13:27:45 +0530

    Delete src directory

--- src/main/java/com/yaksha/training/job/repository/JobRepository.java
+++
@@ -1,7 +0,0 @@
-package com.yaksha.training.job.repository;
-
-import com.yaksha.training.job.entity.Job;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JobRepository extends JpaRepository<Job, Long> {
-}
